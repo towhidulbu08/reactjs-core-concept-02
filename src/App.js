6397339d70996a62ad3be21666edbf0d7ba969b3
towhidulbu08 <towhidulislam26121997@gmail.com>
2023-11-27 16:45:27 +0600

    now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const n=345344;
+const singers=[
+  {name:"Dr Mahfuz",job:'Singer'},
+  {name:"Eva Rahman",job:'Singer2'},
+  {name:"Agun",job:'sopno'},
+  {name:"shuvro",job:'pathor'}
+]
+
+const singerStyle={
+  color:'purple',
+  backgroundColor:'white'
+
+}
 
 function App() {
+  const nayoks=['Rubel','BappaRaz','Kubel','Josim','Salman Shah','Riaz','Anwar']
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok=> <li>{nayok}</li> )
+      }
+      {
+        // nayoks.map(nayok=><Person name={nayok}></Person>)
+      }
+      {
+        singers.map(singer=> <Person name={singer.name}></Person>)
+      }
+      {/* <Person name={nayoks[0]} nayika="Mosumi"></Person>
+      <Person name={nayoks[1]} nayika="chdeka"></Person>
+      <Person name={nayoks[2]}nayika="kopila"></Person> */}
+    
+      <h5>New Component!!!</h5>
+      <p id="totocompany">rock mama ,React Mama</p>
+      <Friend movie='singham' phone='01777'></Friend>
+      <Friend phone="01999"></Friend>
+     
     </div>
   );
+}
+function Person(props) {
+ 
+  return (
+    <div className="person">
+      <h1>{props.name}</h1>
+      <p>{props.nayika}</p>
+    </div>
+  );
+}
+
+function Friend(props){
+  console.log(props);
+  return(
+    <div className="container">
+        <h3>Name: {props.movie}</h3>
+        <p>Phone : {props.phone}</p>
+    </div>
+  )
 }
 
 export default App;
